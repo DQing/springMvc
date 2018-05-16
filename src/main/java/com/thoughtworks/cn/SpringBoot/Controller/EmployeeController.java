3cdc372c8c94a31c20ce9def4e4d743f4d58ad3d
DQing <15829258737@163.com>
2018-05-16 19:46:30 +0800

    create mvc fun
    Signed-off-by: DQing <15829258737@163.com>

---
+++ src/main/java/com/thoughtworks/cn/SpringBoot/Controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.cn.SpringBoot.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by douqing on 18-5-16.
+ */
+
+@RestController
+public class EmployeeController {
+
+}
